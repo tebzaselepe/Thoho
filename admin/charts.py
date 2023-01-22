4a1e948e5f703d6623ad9b6f2558bb1d200690c9
tebzaselepe <19845678@Tebza>
2023-01-22 13:31:03 +0200

    styles tweaks

--- admin/charts.py
+++ admin/charts.py
@@ -16,84 +16,87 @@ df = pd.read_csv("data.csv")
 
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
-city = st.sidebar.multiselect(
-    "Select the City:",
-    options=df["CITY"].unique(),
-    default=df["CITY"].unique()
-)
+options = st.sidebar.selectbox('Options', ('Filter Data', 'Search Data'))
+st.sidebar.empty()
+if options == 'Filter Data':
+    city = st.sidebar.multiselect(
+        "Select the City:",
+        options=df["CITY"].unique(),
+        default=df["CITY"].unique()
+    )
 
-policy_type = st.sidebar.multiselect(
-    "Select the Policy Type:",
-    options=df["POLICY_TYPE"].unique(),
-    default=df["POLICY_TYPE"].unique(),
-)
+    policy_type = st.sidebar.multiselect(
+        "Select the Policy Type:",
+        options=df["POLICY_TYPE"].unique(),
+        default=df["POLICY_TYPE"].unique(),
+    )
 
-policy_cover = st.sidebar.multiselect(
-    "Select the Policy Cover:",
-    options=df["POLICY_COVER"].unique(),
-    default=df["POLICY_COVER"].unique(),
-)
+    policy_cover = st.sidebar.multiselect(
+        "Select the Policy Cover:",
+        options=df["POLICY_COVER"].unique(),
+        default=df["POLICY_COVER"].unique(),
+    )
 
-gender = st.sidebar.multiselect(
-    "Select the Gender:",
-    options=df["GENDER"].unique(),
-    default=df["GENDER"].unique()
-)
+    gender = st.sidebar.multiselect(
+        "Select the Gender:",
+        options=df["GENDER"].unique(),
+        default=df["GENDER"].unique()
+    )
 
-paid = st.sidebar.multiselect(
-    "Select the paid the month:",
-    options=df["HAS_PAID"].unique(),
-    default=df["HAS_PAID"].unique()
-)
+    paid = st.sidebar.multiselect(
+        "Select the paid the month:",
+        options=df["HAS_PAID"].unique(),
+        default=df["HAS_PAID"].unique()
+    )
 
-df_selection = df.query(
-    "HAS_PAID == @paid  & POLICY_COVER == @policy_cover &  CITY == @city & POLICY_TYPE == @policy_type & GENDER == @gender"
-)
+    df_selection = df.query(
+        "HAS_PAID == @paid  & POLICY_COVER == @policy_cover &  CITY == @city & POLICY_TYPE == @policy_type & GENDER == @gender"
+    )
 
-# ---- MAINPAGE ----
-st.title(":bar_chart: Sales Dashboard")
-st.markdown("##")
+    # ---- MAINPAGE ----
+    st.title(":bar_chart: Sales Dashboard")
+    st.markdown("##")
 
-# TOP KPI's
-total_sales = int(df_selection["MONTHLY_PREMIUM"].sum())
-# average_rating = round(df_selection["Rating"].mean(), 1)
-# star_rating = ":star:" * int(round(average_rating, 0))
-average_sale_by_transaction = round(df_selection["MONTHLY_PREMIUM"].mean(), 2)
+    # TOP KPI's
+    total_sales = int(df_selection["MONTHLY_PREMIUM"].sum())
+    # average_rating = round(df_selection["Rating"].mean(), 1)
+    # star_rating = ":star:" * int(round(average_rating, 0))
+    average_sale_by_transaction = round(df_selection["MONTHLY_PREMIUM"].mean(), 2)
 
-left_column, right_column = st.columns(2)
-with left_column:
-    st.subheader("Totals Expected:")
-    st.subheader(f"R {total_sales:,}")
-with right_column:
-    st.subheader("Average Rating:")
-    # st.subheader(f"{average_rating} {star_rating}")
-with right_column:
-    st.subheader("Average Sales Per Transaction:")
-    st.subheader(f"R {average_sale_by_transaction}")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.subheader("Totals Expected:")
+        st.subheader(f"R {total_sales:,}")
+    with right_column:
+        st.subheader("Average Rating:")
+        # st.subheader(f"{average_rating} {star_rating}")
+    with right_column:
+        st.subheader("Average Sales Per Transaction:")
+        st.subheader(f"R {average_sale_by_transaction}")
 
-# st.markdown("""---""")
+    # st.markdown("""---""")
 
-# SALES BY PRODUCT LINE [BAR CHART]
-sales_by_product_line = (
-    df_selection.groupby(by=["POLICY_TYPE"]).sum()[["MONTHLY_PREMIUM"]].sort_values(by="MONTHLY_PREMIUM")
-)
-st.dataframe(sales_by_product_line)
-fig_product_sales = px.bar(
-    sales_by_product_line,
-    x="MONTHLY_PREMIUM",
-    y=sales_by_product_line.index,
-    orientation="h",
-    title="<b>Sales by Product Line</b>",
-    color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
-    template="plotly_white",
-)
-fig_product_sales.update_layout(
-    plot_bgcolor="rgba(0,0,0,0)",
-    xaxis=(dict(showgrid=False))
-)
-left_column, right_column = st.columns(2)
-# left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
-right_column.plotly_chart(fig_product_sales, use_container_width=True)
+    # SALES BY PRODUCT LINE [BAR CHART]
+    sales_by_product_line = (
+        df_selection.groupby(by=["POLICY_TYPE"]).sum()[["MONTHLY_PREMIUM"]].sort_values(by="MONTHLY_PREMIUM")
+    )
+    st.dataframe(sales_by_product_line)
+    fig_product_sales = px.bar(
+        sales_by_product_line,
+        x="MONTHLY_PREMIUM",
+        y=sales_by_product_line.index,
+        orientation="h",
+        title="<b>Sales by Product Line</b>",
+        color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
+        template="plotly_white",
+    )
+    fig_product_sales.update_layout(
+        plot_bgcolor="rgba(0,0,0,0)",
+        xaxis=(dict(showgrid=False))
+    )
+    left_column, right_column = st.columns(2)
+    # left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
+    right_column.plotly_chart(fig_product_sales, use_container_width=True)
 
 # <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
 # <script defer src="https://pyscript.net/latest/pyscript.js"></script>
--- app.py
+++ app.py
@@ -3,51 +3,19 @@ from streamlit_extras.app_logo import add_logo
 from streamlit_login_auth_ui.widgets import __login__
 import streamlit_book as stb
 
-
 __login__obj = __login__(auth_token = "pk_test_9J565P8EY847JAKEKF7F9JX94HJN",
                     company_name = "TFS",
                     width = 200, height = 250,
                     logout_button_name = 'Logout', hide_menu_bool = True,
                     hide_footer_bool = True,
                     lottie_url = 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
-add_logo("logo-250.png", height=200)
+
+add_logo("logo-250.png", height=20)
+
 LOGGED_IN= __login__obj.build_login_ui()
 username= __login__obj.get_username()
 
 import streamlit.components.v1 as components
-
-# bootstrap 4 collapse example
-components.html(
-    """
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="#">Fixed navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-        </ul>
-        <form class="form-inline mt-2 mt-md-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
-    """,
-    height=80
-)
 
 def show_book():
     stb.set_book_config(
@@ -71,10 +39,10 @@ def show_book():
                 }
     )
 
-if LOGGED_IN is not True:
-    st.stop()
-else:
+if LOGGED_IN == True:
     show_book()
+else:
+    st.sidebar.empty()
+    st.stop()
     # st.write(username)
-   # stb.render_file('clients.py')
-   
+    # stb.render_file('clients.py')
